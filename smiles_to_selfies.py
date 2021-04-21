dd875c1dfeed02c89f23d9dd7e88cd4fa12392fe
Ghureg <33948288+Ghureg@users.noreply.github.com>
2021-04-20 21:37:41 -0400

    Add files via upload

---
+++ smiles_to_selfies.py
@@ -0,0 +1,17 @@
+import selfies
+import sys
+
+def convert(smile_string):
+    return selfies.encoder(str(smile_string.strip()),print_error=True)
+def main(file):
+    with open(file, 'r') as f:
+        selfies_list = []
+        for i, line in enumerate(f):
+            selfies_list.append(convert(line))
+    with open("SMILES_" + file, 'w+') as f:
+        for i in selfies_list:
+            f.write(str(i) + "\n")
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    main(file)
