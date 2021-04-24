4d0c1bd9b5d7b62b09ccd320c115ee32868e25fe
rachaelrogan <47510726+rachaelrogan@users.noreply.github.com>
2021-04-24 16:25:02 -0400

    I think I fixed the file structure...

---
+++ RNN_RL_SELFIES/smiles_to_selfies.py
@@ -0,0 +1,21 @@
+import selfies
+import sys
+
+def convert(smile_string):
+    return selfies.encoder(str(smile_string.strip()),print_error=True)
+def main(file):
+    with open(file, 'r') as f:
+        selfies_list = []
+        for i, line in enumerate(f):
+            selfies_list.append(convert(line))
+    for i in selfies_list:
+        if(i == None):
+            selfies_list.remove(i)
+    with open("SELFIES_" + file, 'w+') as f:
+        for i in selfies_list:
+            if i != None:
+                f.write(str(i) + "\n")
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    main(file)
